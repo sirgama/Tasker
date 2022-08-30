ce6c4ac3523d90c8c3b9c4e4f8e7f394401a220e
sirgama <contact@sirgama.dev>
2022-08-30 15:55:29 +0300

    configure template base directory

--- tasker/settings.py
+++ tasker/settings.py
@@ -61,10 +61,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tasker.urls'
 
+# Templates Directory
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
