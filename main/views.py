bc42bd13b198116076fe4dd61fcd6d921a02cac2
sirgama <contact@sirgama.dev>
2022-08-30 15:38:00 +0300

    configure homepage url path and view function

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+    
+    context = {
+        
+    }
+    return render(request, 'main/home.html', context)
